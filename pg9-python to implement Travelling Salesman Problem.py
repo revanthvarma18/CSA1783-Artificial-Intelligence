63895fe68a1eb687355409c099783e233b509c19
revanthvarma18 <112714751+revanthvarma18@users.noreply.github.com>
2023-08-26 13:18:11 +0530

    pg9-python to implement Travelling Salesman Problem.py

--- pg9-python to implement Travelling Salesman Problem.py
+++ pg9-python to implement Travelling Salesman Problem.py
@@ -1,41 +1,31 @@
-from sys import maxsize
-from itertools import permutations
-V = 4
-
-def travellingSalesmanProblem(graph, s):
-
-    # store all vertex apart from source vertex
-    vertex = []
-    for i in range(V):
-        if i != s:
-            vertex.append(i)
-
-    # store minimum weight Hamiltonian Cycle
-    min_path = maxsize
-    next_permutation=permutations(vertex)
-    for i in next_permutation:
-
-        # store current Path weight(cost)
-        current_pathweight = 0
-
-        # compute current path weight
-        k = s
-        for j in i:
-            current_pathweight += graph[k][j]
-            k = j
-        current_pathweight += graph[k][s]
-
-        # update minimum
-        min_path = min(min_path, current_pathweight)
-
-    return min_path
-
-
-# Driver Code
-if __name__ == "__main__":
-
-    # matrix representation of graph
-    graph = [[0, 10, 15, 20], [10, 0, 35, 25],
-            [15, 35, 0, 30], [20, 25, 30, 0]]
-    s = 0
-    print(travellingSalesmanProblem(graph, s))
+import itertools
+def calculate_distance(points, order):
+    total_distance = 0
+    num_points = len(order)
+    for i in range(num_points):
+        point1 = points[order[i]]
+        point2 = points[order[(i + 1) % num_points]]
+        total_distance += calculate_distance_between_points(point1, point2)
+    return total_distance
+def calculate_distance_between_points(point1, point2):
+    return ((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2) ** 0.5
+def tsp_bruteforce(points):
+    min_distance = float('inf')
+    optimal_order = None
+    num_points = len(points)
+    for order in itertools.permutations(range(num_points)):
+        distance = calculate_distance(points, order)
+        if distance < min_distance:
+            min_distance = distance
+            optimal_order = order
+    return min_distance, optimal_order
+points = {
+    'A': (0, 0),
+    'B': (1, 2),
+    'C': (4, 1),
+    'D': (2, 4)
+}
+min_distance, optimal_order = tsp_bruteforce(list(points.values()))
+optimal_path = [list(points.keys())[i] for i in optimal_order]
+print("Optimal Path:", optimal_path)
+print("Min Distance:", min_distance)
